a231e26b2af8b7b42628fb850dde703766e58adc
andyay-nj <knajaeng@gmail.com>
2020-05-27 18:20:16 +0900

    try strategy DIY mode

--- index.ts
+++ index.ts
@@ -1,5 +1,36 @@
 import { MakeBurger as decorator } from './decorator';
 import { Main as observer } from './observer';
+import { Main as strategy, Card, Cash } from './strategy';
+import { Main as factory } from './factory';
+
+const print = (name: string, func: any) => {
+    console.log(`------ < ${name}_pattern > ------`);
+    func.main();
+    console.log(`---------------------------------`);
+    console.log(` `);
+}
+
+const deco = new decorator();
+print('decorator', deco);
+
+const obsvr = new observer();
+print('observer', obsvr);
+
+const strtgy = new strategy();
+console.log(`------ < strategy_pattern > ------`);
+strtgy.main('나정', new Card());
+strtgy.main('나정', new Cash());
+console.log(`----------------------------------`);
+console.log(` `);
+
+
+const fctry = new factory();
+print('factory', fctry);
+
+/**
+
+import { MakeBurger as decorator } from './decorator';
+import { Main as observer } from './observer';
 import { Main as strategy } from './strategy';
 import { Main as factory } from './factory';
 
@@ -21,3 +52,5 @@ print('strategy', strtgy);
 
 const fctry = new factory();
 print('factory', fctry);
+
+ */
--- strategy.ts
+++ strategy.ts
@@ -1,4 +1,38 @@
 interface PaymentPolicy {
+    pay(customer: string): void;
+}
+
+class Customer {
+    changePayment(customer: string, payment: PaymentPolicy): void {
+        payment.pay(customer);
+    }
+}
+
+export class Card implements PaymentPolicy {
+    pay(customer: string): void {
+        console.log(`${customer} 💳카드긁기`);
+    }
+}
+
+export class Cash implements PaymentPolicy {
+    pay(customer: string): void {
+        console.log(`${customer} 💵현금내기`);
+    }
+}
+
+export class Main {
+    customer = new Customer();
+    // card = new Card();
+    // cash = new Cash();
+
+    main(customer: string, payment: PaymentPolicy): void {
+        this.customer.changePayment(customer, payment);
+    }
+}
+
+/**
+
+interface PaymentPolicy {
     pay(): void;
 }
 
@@ -31,3 +65,5 @@ export class Main {
         this.customer.changePayment(this.cash);
     }
 }
+
+ */
