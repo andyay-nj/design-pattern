297899dc9cc20f2ed55c7ceaa84b517118e0f833
andyay-nj <knajaeng@gmail.com>
2020-05-30 13:40:19 +0900

    fix decorator

--- decorator.ts
+++ decorator.ts
@@ -1,49 +1,44 @@
 //최상위 햄버거
 abstract class Hamburger {   
     //장식 할 대상과 장식자가 공통으로 가져야 할 인터페이스를 정의
-    abstract add_ingredient(): string;
+    abstract add_ingredient(ingredient?: Hamburger): string;
 }
 
 //장식할 빵
 class Bread extends Hamburger {
-    public add_ingredient(): string {
-        return '기본빵';
+    public add_ingredient(ingredient: Hamburger): string {
+        return `기본빵 + ${ingredient.add_ingredient()}`;
     }
 }
 
 abstract class Ingredient extends Hamburger {
     public abstract add_ingredient(): string;
+    
 }
 
 class Bulgogi extends Ingredient {
-    constructor(private burger: string) {
-        super();
-    }
     public add_ingredient(): string {
-        return `${this.burger} + 불고기패티`;
+        return `불고기패티`;
     }
 }
 
 class Shrimp extends Ingredient {
-    constructor(private burger: string) {
-        super();
-    }
     public add_ingredient(): string {
-        return `${this.burger} + 새우패티`;
+        return `새우패티`;
     }
 } 
 
 export class MakeBurger {
-    bread = new Bread().add_ingredient();
-    bulgogi_burger =  new Bulgogi(this.bread).add_ingredient();
-    shrimp_burger =  new Shrimp(this.bread).add_ingredient();
-    bulgogi_shrimp_burger = new Shrimp(this.bulgogi_burger).add_ingredient();
+    bread = new Bread();
+    bulgogi_burger =  this.bread.add_ingredient(new Bulgogi());
+    shrimp_burger =  this.bread.add_ingredient(new Shrimp());
+    // bulgogi_shrimp_burger =  this.bread.add_ingredient(new Bulgogi());
     
     public main(): void {
         console.log('1',this.bread);
         console.log('2',this.bulgogi_burger);
         console.log('3',this.shrimp_burger);
-        console.log('4',this.bulgogi_shrimp_burger);
+        // console.log('4',this.bulgogi_shrimp_burger);
         /**
          * 1 '기본빵'
          * 2 '기본빵+불고기패티'
@@ -52,4 +47,3 @@ export class MakeBurger {
          */
     }
 }
-
